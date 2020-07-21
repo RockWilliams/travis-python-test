59ae48d43036246c20faa3f7595a367cc5c2f286
RockW <rock7699@gmail.com>
2020-07-21 14:07:27 -0700

    test area of circle sanity

--- functions/math_functions.py
+++ functions/math_functions.py
@@ -5,7 +5,7 @@ PI = math.pi
 
 def area_of_circle(radius):
     """ determines the area of a circle with a 'radius' input """
-    return radius
+    return None if radius <= 0 else PI * radius ** 2
 
 def surface_area_of_cube(side):
     """ determines the surface area of a cube with a 'side' input """
--- tests/test_math_functions.py
+++ tests/test_math_functions.py
@@ -9,9 +9,9 @@ class KnowledgeBase(unittest.TestCase):
     def test_area_of_circle_sanity(self):
         """ area_of_circle(1) """
         # Captue the results of the function
-        result = math_functions.area_of_circle(1)
+        result = math_functions.area_of_circle(10)
         # Check for exptected output
-        expected = 1
+        expected = 314.1592653589793
         self.assertEqual(expected, result)
 
 
