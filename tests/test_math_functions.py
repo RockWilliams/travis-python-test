881af44b51f8cf748ca78c8169255c71da09a690
RockW <rock7699@gmail.com>
2020-07-21 14:12:55 -0700

    adds test surface area of cube

--- tests/test_math_functions.py
+++ tests/test_math_functions.py
@@ -9,9 +9,15 @@ class KnowledgeBase(unittest.TestCase):
     def test_area_of_circle_sanity(self):
         """ area_of_circle(1) """
         # Captue the results of the function
-        result = math_functions.area_of_circle(9)
+        result = math_functions.area_of_circle(10)
         # Check for exptected output
         expected = 314.1592653589793
+        self.assertEqual(expected, result)
+
+    def test_surface_area_of_cube(self):
+        """ area of cube """
+        result = math_functions.surface_area_of_cube(1)
+        expected = 1
         self.assertEqual(expected, result)
 
 
